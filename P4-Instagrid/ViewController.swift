a01a5e98a22d4ba93eb713bdc3891bda48c34d6d
Adri3n89 <adrien@perea.be>
2021-04-22 17:39:11 +0200

    Update ViewController.swift

--- P4-Instagrid/ViewController.swift
+++ P4-Instagrid/ViewController.swift
@@ -7,17 +7,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet var myButton: [UIButton]!
     @IBOutlet weak var upLeft: UIButton!
     @IBOutlet weak var upRight: UIButton!
     @IBOutlet weak var downLeft: UIButton!
     @IBOutlet weak var downRight: UIButton!
+    var picker: UIImagePickerController = UIImagePickerController()
+    var selectedImageButton: UIButton?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        picker.delegate = self
         buttonPressed(myButton[1])
+    }
+
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+// swiftlint:disable:next line_length
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+        if let photo = info[.originalImage] as? UIImage {
+            selectedImageButton?.setImage(photo, for: .normal)
+            selectedImageButton?.imageView?.contentMode = .scaleAspectFill
+        }
+        picker.dismiss(animated: true, completion: nil)
     }
 
     func setupButton(sender: UIButton) {
@@ -53,6 +68,17 @@ class ViewController: UIViewController {
             upRight.isHidden = false
             downLeft.isHidden = false
             downRight.isHidden = false
+        default:
+            return
+        }
+    }
+
+    @IBAction func selectImage(_ sender: UIButton) {
+        switch sender.tag {
+        case 1, 2, 3, 4:
+            selectedImageButton = sender
+            picker.sourceType = .camera
+            present(picker, animated: true, completion: nil)
         default:
             return
         }
