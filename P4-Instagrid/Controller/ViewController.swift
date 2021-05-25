03f510eea89583dc5ca96d3135c0d3a83ec4d655
Adri3n89 <adrien@perea.be>
2021-05-25 18:57:34 +0200

    update func initpicker et init gesture

--- P4-Instagrid/Controller/ViewController.swift
+++ P4-Instagrid/Controller/ViewController.swift
@@ -14,19 +14,13 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     @IBOutlet weak var gridView: UIView!
     var picker: UIImagePickerController = UIImagePickerController()
     var selectedImageButton: UIButton?
+    private let duration: Double = 1
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        picker.delegate = self
-        picker.sourceType = .photoLibrary
-        picker.allowsEditing = true
+        initPicker()
+        initGesture()
         buttonPressed(myButton[1])
-        let leftSwipeGesture = UISwipeGestureRecognizer(target: self, action: #selector(swipeByUser(_:)))
-        leftSwipeGesture.direction = .left
-        let upSwipeGesture = UISwipeGestureRecognizer(target: self, action: #selector(swipeByUser(_:)))
-        upSwipeGesture.direction = .up
-        self.view.addGestureRecognizer(leftSwipeGesture)
-        self.view.addGestureRecognizer(upSwipeGesture)
     }
 
     @IBAction func buttonPressed(_ sender: UIButton) {
@@ -46,16 +40,31 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
 
     @objc private func swipeByUser(_ sender: UISwipeGestureRecognizer) {
         if UIDevice.current.orientation.isLandscape && sender.direction == .left {
-            UIView.animate(withDuration: 0.3) {
+            UIView.animate(withDuration: duration) {
                 self.gridView.transform = CGAffineTransform(translationX: -self.view.frame.width * 2, y: 0)
             }
             shareImage()
         } else if (UIDevice.current.orientation.isPortrait || UIDevice.current.orientation == .unknown) && sender.direction == .up {
-            UIView.animate(withDuration: 0.3) {
+            UIView.animate(withDuration: duration) {
                 self.gridView.transform = CGAffineTransform(translationX: 0, y: -self.view.frame.height * 2)
             }
             shareImage()
         }
+    }
+
+    private func initPicker() {
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        picker.allowsEditing = true
+    }
+
+    private func initGesture() {
+        let leftSwipeGesture = UISwipeGestureRecognizer(target: self, action: #selector(swipeByUser(_:)))
+        leftSwipeGesture.direction = .left
+        let upSwipeGesture = UISwipeGestureRecognizer(target: self, action: #selector(swipeByUser(_:)))
+        upSwipeGesture.direction = .up
+        self.view.addGestureRecognizer(leftSwipeGesture)
+        self.view.addGestureRecognizer(upSwipeGesture)
     }
 
     private func shareImage() {
@@ -64,22 +73,10 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         let activityVC = UIActivityViewController(activityItems: items, applicationActivities: nil)
         present(activityVC, animated: true)
         activityVC.completionWithItemsHandler = { (_, _, _, _) in
-            UIView.animate(withDuration: 0.3) {
+            UIView.animate(withDuration: self.duration) {
                 self.gridView.transform = .identity
             }
         }
-    }
-
-    internal func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-        picker.dismiss(animated: true, completion: nil)
-    }
-
-    internal func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
-        if let photo = info[.editedImage] as? UIImage {
-            selectedImageButton?.setImage(photo, for: .normal)
-            selectedImageButton?.imageView?.contentMode = .scaleAspectFill
-        }
-        picker.dismiss(animated: true, completion: nil)
     }
 
     private func setupButton(sender: UIButton) {
@@ -113,4 +110,15 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         }
     }
 
+    internal func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+
+    internal func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+        if let photo = info[.editedImage] as? UIImage {
+            selectedImageButton?.setImage(photo, for: .normal)
+            selectedImageButton?.imageView?.contentMode = .scaleAspectFill
+        }
+        picker.dismiss(animated: true, completion: nil)
+    }
 }
